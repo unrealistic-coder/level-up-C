d4476479bdfe1d4e044ede38fe08ab25bc90f125
Harish Karthik <hkarthik1208@gmail.com>
2024-10-04 12:41:26 +0530

    trying string constants

---
+++ src/functions.c
@@ -0,0 +1,16 @@
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+void callme(char s[]){
+    printf("%s",s);
+}
+
+
+int main(){
+    char *st = "aeragt";
+    char sst[] = "aeragt";
+    callme(st);
+    callme(sst);
+return 0;
+}
